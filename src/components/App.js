9f2a5118defa35c3e525cef10084b636e1115365
ArashMidos <arshiamidos@gmail.com>
2019-05-13 14:12:53 +0430

    build to product

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {Switch, Redirect, Route} from 'react-router-dom';
 import BaseComponent from './BaseComponent';
-import NotFound from './NotFound'
 import Home from './Home'
 import '../assets/bootstrap/css/grid.min.css'
 import '../assets/scss/app.scss'
@@ -17,8 +16,7 @@ class App extends BaseComponent {
                 <div className="main-container">
                     <div className="">
                         <Switch>
-                            <Route exact path={"/"}     render={() => (<Home/>)}/>
-                            <Route       path={"*"}     render={() => (<NotFound/>)}/>
+                            <Route path={"*"} render={() => (<Home/>)}/>
                         </Switch>
                     </div>
                 </div>
